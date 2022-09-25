ec8bd3d27698639af0dd16863c5e3a9e7ad5c6c4
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-09-25 10:57:27 +0200

    Variación como verdadero torero

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 90)
         music.playTone(262, music.beat(BeatFraction.Whole))
-    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) > 14) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
         music.playTone(349, music.beat(BeatFraction.Whole))
     } else {
