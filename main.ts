0356f21d74b484590d9ebc85b08352fc7f88a27f
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-09-25 09:33:21 +0200

    Programa que describe la tarea 5

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+basic.showString("Hola")
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 125)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(200)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 125)
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    }
+    basic.pause(200)
 })
