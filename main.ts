97c5bba2effa1a112df58d1546a6fb72f4202eea
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-09-25 09:43:28 +0200

    Mejoras. Que se pare cuando la distancia sea exactamente 10 cm

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 basic.showString("Hola")
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 10) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 125)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 90)
         music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.pause(200)
-    } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 125)
+    } else if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
         music.playTone(349, music.beat(BeatFraction.Whole))
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
     }
-    basic.pause(200)
+    basic.pause(100)
 })
